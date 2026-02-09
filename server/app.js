ef842c160b5f822892fbf24a8d1b6cb70e1d0d39
Robert-Beckett <robert.beckett@onion.red>
2026-02-09 13:00:44 -0400

    Isolate route mounting.
    - Modified app.js such that it only runs a mount function written in
      route/index.js. All route mounting will be added to app in this
      function, so we don't need to bloat out the main `app.js` file as we
      modify routes.

--- server/app.js
+++ server/app.js
@@ -2,20 +2,9 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-const wishlistsRoutes = require('./routes/wishlist')
-const caseworkerRoutes = require('./routes/caseworker')
+const { mountRoutes } = require('./routes/index');
 
-
-//routes for users to view wishlists
-app.use('/', wishlistsRoutes)
-
-//routes for caseworkers portal
-app.use('/caseworker', caseworkerRoutes)
-
-
-// app.get('/', (req, res) => {
-//     res.send('Hey there. Peach Gardenia Team is working actively on this project. Stay tuned for updates!');
-// });
+mountRoutes(app);
 
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`);
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,21 +1,18 @@
-const express = require('express');
-const app = express();
- 
-
 const wishlistsRoutes = require('./wishlist')
 const caseworkerRoutes = require('./caseworker')
 
-//routes for users to view wishlists
-app.use('/', wishlistsRoutes)
+function mountRoutes(app) {
+  //routes for users to view wishlists
+  app.use('/', wishlistsRoutes)
 
-//routes for caseworkers portal
-app.use('/caseworker', caseworkerRoutes)
+  //routes for caseworkers portal
+  app.use('/caseworker', caseworkerRoutes)
 
-//Future possible routes
-// A route to refresh the wishlists 
-// An admin route for developers to view all caseworker accounts 
+  //Future possible routes
+  // A route to refresh the wishlists 
+  // An admin route for developers to view all caseworker accounts 
+}
 
-app.listen(port, () => {
-    console.log(`Server listening at http://localhost:${port}`);
-});
-
+module.exports = {
+  mountRoutes,
+}
