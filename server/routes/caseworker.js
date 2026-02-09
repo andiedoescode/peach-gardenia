4cb2e94f39200ae74999ea820011721f3fba9509
MiliMade <milimadeemail@gmail.com>
2026-02-09 01:16:01 -0400

    Made minor changes to routes

--- server/routes/caseworker.js
+++ server/routes/caseworker.js
@@ -5,56 +5,55 @@ router.use(express.urlencoded({extended:true}))
 
 
 //Caseworker portal where they can sign in or request an account
-router.get('/', (req,res)=>{
-  res.send("Welcome to caseworker portal homepage")
-})
+router
+  .get('/', (req,res)=>{
+    res.send("Welcome to caseworker portal homepage")
+  })
 
 //Route to submit login information
-router.post('/', (req,res)=>{
-   res.send("Caseworker is now logged in")
-})
+  .post('/', (req,res)=>{
+    res.send("Caseworker is now logged in")
+  })
 
 //Caseworker contact form - get route
-router.get('/form', (req, res)=>{
-  res.send("Contact form for caseworkers to sign up")
-})
+  .get('/form', (req, res)=>{
+    res.send("Contact form for caseworkers to sign up")
+  })
 
 //Caseworker contact form - post route
-router.post('/form', (req,res)=>{
-  res.send("Account request submitted")
-})
+  .post('/form', (req,res)=>{
+    res.send("Account request submitted")
+  })
 
+  //All wishlists that are related to caseworker id
+  .get('/:id', (req,res)=>{
+    res.send("Here are all the wishlists made by this caseworker")
+  })
 
+  //create a wishlist
+  .post('/:id', (req,res)=>{
+    res.send("New wishlist created")
+  })
 
-//All wishlists that are related to caseworker id
-router.get('/:id', (req,res)=>{
-  res.send("Here are all the wishlists made by this caseworker")
-})
+  //Get one wishlist
+  .get('/wishlist/:id', (req,res)=>{
+   res.send("Viewing one wishlist in caseworker portal") 
+  })
 
-//Get one wishlist
-router.get('/wishlist/:id', (req,res)=>{
-  res.send("Viewing one wishlist in caseworker portal") 
-})
-
-//Edit wishlist - GET route to populate fields
-router.get('/wishlist/:id/edit', (req,res)=>{
-  res.send("You're now editing a list")
-})
+  //Edit wishlist - GET route to populate fields
+  .get('/wishlist/:id/edit', (req,res)=>{
+    res.send("You're now editing a list")
+  })
 
 //Update wishlist - PUT route
-router.put('/wishlist/:id', (req,res)=>{
-  res.send("Wishlist updated!")
-})
-
-//create a wishlist
-router.post('/:id', (req,res)=>{
-  res.send("New wishlist created")
-})
+  .put('/wishlist/:id', (req,res)=>{
+    res.send("Wishlist updated!")
+  })
 
 //delete a wishlist
-router.delete('/wishlist/:id', (req,res)=>{
-  res.send("Wishlist deleted")
-})
+  .delete('/wishlist/:id', (req,res)=>{
+    res.send("Wishlist deleted")
+  })
 
 
 
--- server/routes/wishlist.js
+++ server/routes/wishlist.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req,res)=>{
-  res.send("All wishlists")
-})
+router
+  .get('/', (req,res)=>{
+    res.send("All wishlists")
+  })
 
-router.get('/wishlist/:id', (req,res)=>{
-  res.send("Viewing one wishlist")
-})
+  .get('/wishlist/:id', (req,res)=>{
+    res.send("Viewing one wishlist")
+  })
 
 module.exports = router
